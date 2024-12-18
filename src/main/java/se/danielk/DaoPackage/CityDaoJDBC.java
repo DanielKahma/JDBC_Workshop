109bb1bf12dd0dd4bcda085868ac0f6ae2a48ec1
Daniel.Kahma <you@example.com>
2024-12-18 11:19:45 +0100

    Second Commit

--- src/main/java/se/danielk/DaoPackage/CityDaoJDBC.java
+++ src/main/java/se/danielk/DaoPackage/CityDaoJDBC.java
@@ -1,4 +1,66 @@
 package se.danielk.DaoPackage;
 
+import se.danielk.City;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 public class CityDaoJDBC implements CityDao {
+
+    Connection connection;
+
+    {
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/world",
+                    "root",
+                    "0611");
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public City findById(int id) {
+        String query = "SELECT * FROM city WHERE id = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(query)){
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<City> findByCode(String code) {
+        String query = "SELECT * FROM city WHERE CountryCode = ?";
+        return List.of();
+    }
+
+    @Override
+    public List<City> findByName(String name) {
+        String query = "SELECT * FROM city WHERE name = ?";
+        return List.of();
+    }
+
+    @Override
+    public List<City> findAll() {
+        String query = "SELECT * FROM city";
+        return List.of();
+    }
+
+    @Override
+    public City add(City city) {
+        return null;
+    }
+
+    @Override
+    public int delete(City city) {
+        return 0;
+    }
 }
--- src/main/java/se/danielk/Main.java
+++ src/main/java/se/danielk/Main.java
@@ -4,12 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) throws SQLException {
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_db", "root", "0611");
+public class Main {
+    public static void main(String[] args) {
 
         }
     }
